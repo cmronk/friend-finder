e8c90e1a46934316a530a27120468be621ab76f5
cronk <ChristineRonk@Stevens-MacBook-Pro.local>
2018-07-06 18:34:49 -0500

    updated styling and form validation to prevent blank answers

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,8 +2,8 @@
 
 var friendData = [
 {
-    "name":"Harry",
-    "photo":"https://nerdist.com/wp-content/uploads/2017/01/harry-potter.jpg",
+    "name":"Thing 1 AND Thing 2",
+    "photo":"https://vignette.wikia.nocookie.net/seuss/images/4/4d/38d604baa084bd18baa9c744201fc4b4.jpg/revision/latest?cb=20170612184234",
     "scores":[
         2,
         1,
@@ -18,14 +18,14 @@ var friendData = [
       ]
   },
   {
-    "name":"Hermione",
-    "photo":"https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Hermione_Granger_poster.jpg/220px-Hermione_Granger_poster.jpg",
+    "name":"Horton",
+    "photo":"https://vignette.wikia.nocookie.net/seuss/images/4/47/Horton.gif/revision/latest?cb=20121109204606",
     "scores":[
         3,
         1,
         2,
         4,
-        4,
+        1,
         1,
         2,
         1,
@@ -34,14 +34,14 @@ var friendData = [
       ]
   },
   {
-    "name":"Ron",
-    "photo":"https://pbs.twimg.com/profile_images/639082534370975744/dlgC-dga_400x400.jpg",
+    "name":"Sam I Am",
+    "photo":"https://vignette.wikia.nocookie.net/seuss/images/3/38/Sam-I-Am_1.gif/revision/latest?cb=20130805123312",
     "scores":[
         5,
         1,
         1,
         1,
-        5,
+        4,
         1,
         3,
         5,
@@ -49,6 +49,40 @@ var friendData = [
         5
       ]
   },
+  {
+    "name":"The Grinch",
+    "photo":"https://upload.wikimedia.org/wikipedia/en/7/73/The_Grinch.png",
+    "scores":[
+        3,
+        2,
+        5,
+        5,
+        2,
+        1,
+        5,
+        4,
+        4,
+        5
+      ]
+  },
+  {
+    "name":"The Cat in the Hat",
+    "photo":"https://www.thewrap.com/wp-content/uploads/2018/01/catinthehat.jpg",
+    "scores":[
+        4,
+        2,
+        1,
+        5,
+        2,
+        4,
+        3,
+        1,
+        5,
+        3
+      ]
+  },
+
+
 
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,13 +12,17 @@ module.exports = function(app) {
 
     // POST requests
     app.post("/api/friends", function(req, res){
+        // user form input
         var newEntry = req.body;
 
+        // scores array from form input
         var matchingScores = newEntry.scores;
 
+        // declared variables for our match and comparison is an arbitrary number to compare
         var matchedName = "";
         var matchedPic = "";
         var comparison = 100;
+
 
         for (var i = 0; i < friendData.length; i++) {
             var difference = 0;
